95041881c9048b9fbe656f57ae39f11183bb8008
Li He <80284104+Li-8023@users.noreply.github.com>
2024-04-17 17:43:27 +0800

    edit the code

--- ScoreKeeper/ScoreKeeper/app.js
+++ ScoreKeeper/ScoreKeeper/app.js
@@ -11,6 +11,7 @@ const p2 = {
 
 const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#playto');
+
 let winningScore = 3;
 let isGameOver = false;
 
